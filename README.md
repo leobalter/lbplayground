4adc741ee4f22ba67845fe9283e09b546322d803
Leo Balter <301201+leobalter@users.noreply.github.com>
2024-04-23 11:49:45 -0700

    generates license file

--- README.md
+++ README.md
@@ -1,28 +1,18 @@
-# My Package
+# Playground code
+
+* It generates an MIT License file. *
 
 [![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
 
 ## Description
 
-A brief description of your package goes here.
+Generate an MIT LICENSE file, it takes an argument for the filename. If the file exists this might just output the content to the stdout.
 
-## Features
-
-- Feature 1
-- Feature 2
-- Feature 3
-
-## Installation
-
-Describe how to install your package here.
-
-## Usage
-
-Provide instructions on how to use your package here.
+Locally, run `node index.js {filename}`. Filename defaults to `LICENSE`.
 
 ## Contributing
 
-Explain how others can contribute to your project here.
+This is an experimental playground.
 
 ## License
 
@@ -30,5 +20,4 @@ This project is licensed under the [MIT License](https://opensource.org/licenses
 
 ## Contact
 
-- Email: your-email@example.com
-- GitHub: [Your GitHub Username](https://github.com/your-username)
+- GitHub: [leobalter](https://github.com/leobalter)
--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-console.log('Hello world!');
+import fs from 'fs';
+import { createInterface } from 'readline';
+import packageFile from './package.json' with { type: "json" };
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const year = new Date().getFullYear();
+const name = packageFile.author;
+
+const filename = process.argv[2];
+
+const licenseText = `
+# MIT License
+
+Copyright (c) ${year} ${name}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+`;
+
+function promptQuestion(question) {
+    return new Promise((resolve) => {
+        readline.question(question, (answer) => {
+            resolve(answer.toLowerCase());
+        });
+    });
+}
+
+if (filename) {
+    if (fs.existsSync(filename)) {
+        const answer = await promptQuestion(`File ${filename} already exists. Do you want to overwrite it? (y/n)`);
+        if (answer === 'y' || answer == '') {
+            fs.writeFileSync(filename, licenseText);
+            console.log(`File ${filename} was successfully updated.`);
+            process.exit(0);
+        } else {
+            const printAnswer = await promptQuestion('Do you want to print the license to stdout? (y/n)');
+            if (printAnswer.toLowerCase() === 'y' || printAnswer == '') {
+                console.log(licenseText);
+            }
+            process.exit(0);
+        }
+    } else {
+        fs.writeFileSync(filename, licenseText);
+        console.log(`File ${filename} was successfully created.`);
+        process.exit(0);
+    }
+} else {
+    console.log(licenseText);
+}
